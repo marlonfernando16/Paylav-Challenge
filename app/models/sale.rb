bec7ce2f125f759b3372219834e3eed5a66d3b21
marlon.fernando16 <marlon.fernando16@outlook.com>
2020-09-13 08:04:43 +0000

    Validate sale fields

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,4 @@
 class Sale < ApplicationRecord
+    include ActiveModel::ForbiddenAttributesProtection
+    validates :purchaser_name, :item_description, :item_price, :purchase_count, :merchant_address, :merchant_name, presence: true
 end
