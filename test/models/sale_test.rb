18e1c8f0824718628bc36f443d5b00c01adba1d9
marlon.fernando16 <marlon.fernando16@outlook.com>
2020-09-13 08:05:48 +0000

    Unit test to validade all sale fields

--- test/models/sale_test.rb
+++ test/models/sale_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class SaleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save sale without all required attributes" do
+    sale = Sale.new
+    assert_not sale.save
+  end
 end
