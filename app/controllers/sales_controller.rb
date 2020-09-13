ab2a58a3f43356e38f784ffe80dc4f5b45712dcc
marlon.fernando16 <marlon.fernando16@outlook.com>
2020-09-13 08:09:49 +0000

    Create and refactore functionality to save the input file

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -2,4 +2,16 @@ class SalesController < ApplicationController
   def index
     @sales = Sale.all
   end
+
+  def create
+    Sale.create(sale_params["_json"])
+    respond_to do |format|
+      format.html { redirect_to sales_index_path, turbolinks: false }
+    end
+  end
+
+  def sale_params 
+    params.permit(_json:[:purchaser_name, :item_description, :item_price, :purchase_count, :merchant_address, :merchant_name])
+  end
+
 end
